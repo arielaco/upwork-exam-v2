c9952fb121efec2c3b111e19a8c9b0d95a894ed5
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2023-12-22 05:08:25 -0500

    ADD refactor tests to use Pytests fixtures

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,33 +1,60 @@
+import pytest
+
+from fastapi import status
 from fastapi.testclient import TestClient
 
 from ..src.main import app
 
 
-def test_create_user():
+@pytest.mark.parametrize(
+    "username, password, status_code, happy_path",
+    [
+        (
+            "user@server.com",
+            "password123",
+            status.HTTP_201_CREATED,
+            True,
+        ),
+    ],
+)
+def test_create_user(username, password, status_code, happy_path):
     client = TestClient(app)
     response = client.post(
         "api/v1/sign-up/",
         json={
-            "username": "user@server.com",
-            "password": "password123",
+            "username": username,
+            "password": password,
         },
     )
-    assert response.status_code == 201
-    assert response.json() == {
-        "response": "User created",
-    }
+    if happy_path:
+        assert response.status_code == status_code
+        assert response.json() == {
+            "response": "User created",
+        }
 
 
-def test_login():
+@pytest.mark.parametrize(
+    "username, password, status_code, happy_path",
+    [
+        (
+            "user@server.com",
+            "password123",
+            status.HTTP_200_OK,
+            True,
+        ),
+    ],
+)
+def test_login(username, password, status_code, happy_path):
     client = TestClient(app)
     response = client.post(
         "api/v1/login/",
         json={
-            "username": "user@server.com",
-            "password": "password123",
+            "username": username,
+            "password": password,
         },
     )
-    assert response.status_code == 200
-    assert response.json() == {
-        "token": "jwt",
-    }
+    if happy_path:
+        assert response.status_code == status_code
+        assert response.json() == {
+            "token": "jwt",
+        }
